c0ef70ff427df1098e2f23ae97ef6b0131460827
Melat Worku <76426996+LadyMelz@users.noreply.github.com>
2024-02-19 08:58:34 +0300

    Uploading src files

---
+++ TravelUI/src/attractionData.js
@@ -0,0 +1,51 @@
+export default [
+    {
+        id: 1,
+        img: "abay.png",
+        title: "Abay River",
+        description: ""
+    },
+    {
+        id: 2,
+        img: "afar.png",
+        title: "Dalol Afar",
+        description: ""
+    },
+    {
+        id: 3,
+        img: "ertale.png",
+        title: "Ertale: Active volcano",
+        description: ""
+    },
+    {
+        id: 4,
+        img: "gondar-afar.png",
+        title: "Ancient city: Gondar",
+        description: ""
+    },
+    {
+        id: 5,
+        img: "gondar-castle.png",
+        title: "Gondar castle",
+        description: ""
+    },
+    {
+        id: 6,
+        img: "indigenous-monkeys.png",
+        title: "National Parks",
+        description: ""
+    },
+    {
+        id: 7,
+        img: "lalibela-downwards.png",
+        title: "Laibela Church",
+        description: ""
+    },
+    {
+        id: 8,
+        img: "lalibela-upwards.png",
+        title: "Laibela Church",
+        description: ""
+
+    }
+]
---
+++ TravelUI/src/eventData.js
@@ -0,0 +1,22 @@
+export default [
+    {
+        id: 1,
+        img: "adwa.png",
+        title: "Adwa Del Beal"
+    },
+    {
+        id: 2,
+        img: "greatEthiopianRun.png",
+        title: "The Great Ethiopian Run"
+    },
+    {
+        id: 3,
+        img: "meskel.png",
+        title: "Meskel Beal: the finding of the cross"
+    },
+    {
+        id: 4,
+        img: "timket.png",
+        title: "Timket Beal (Epiphany)"
+    }
+]
